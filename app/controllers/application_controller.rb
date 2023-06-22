69be09ca00819d42f17682be038704bf4907ab4d
KSTMike <katomikako04@gmail.com>
2023-06-22 14:44:20 +0900

    Login & out実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
+class ApplicationController < ActionController::Base
 require "application_responder"
-
-class ApplicationController < ActionController::API
+#include ActionDispatch::Flash
   self.responder = ApplicationResponder
   respond_to :html
-
+  include SessionsHelper
+  before_action :login_required
   include ActionController::MimeResponds
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
